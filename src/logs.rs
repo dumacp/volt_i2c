c690bbb95f6fb1966e9484a0efc832447a8ce28b
Camilo Zapata <camilo.zapata@nebulae.com.co>
2021-09-11 19:52:41 -0500

    release 1.0.0

---
+++ src/logs.rs
@@ -0,0 +1,40 @@
+use log::{LevelFilter, Level, Metadata, Record};
+use syslog::{Facility, Formatter3164, BasicLogger};
+
+struct SimpleLogger;
+
+impl log::Log for SimpleLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+static LOGGER: SimpleLogger = SimpleLogger;
+
+pub fn init_std_log(logstd: bool, appname: &str) -> Result<(), Box<dyn std::error::Error>> {
+    
+    let formatter = Formatter3164 {
+        facility: Facility::LOG_USER,
+        hostname: None,
+        process: appname.to_owned(),
+        pid: 0,
+    };
+
+    if !logstd {
+        let logger = syslog::unix(formatter)?;
+        log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
+            .map(|()| log::set_max_level(LevelFilter::Debug))?
+    } else {
+        log::set_logger(&LOGGER)
+            .map(|()| log::set_max_level(LevelFilter::Debug))?
+    }
+    Ok(())
+}
